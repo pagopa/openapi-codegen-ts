7b57d8f4200f91823f16024d767af711a3acc799
Federico Feroldi <cloudify@users.noreply.github.com>
2018-05-08 15:31:30 +0200

    Skip duplicated imports (#7)

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import yargs = require("yargs");
+
+import { initNunJucksEnvironment, generateApi } from "./gen-api-models";
+
+//
+// parse command line
+//
+
+interface Argv {
+  help?: boolean
+  version?: boolean,
+  specFile?: string,
+  outDir?: string,
+  tsSpecFile?: string
+};
+
+const argv = yargs
+  .option("api-spec", {
+    demandOption: true,
+    string: true,
+    normalize: true,
+    description: "Path to input OpenAPI spec file"
+  })
+  .option("out-dir", {
+    demandOption: true,
+    string: true,
+    normalize: true,
+    description: "Output directory to store generated definition files"
+  })
+  .option("ts-spec-file", {
+    string: true,
+    normalize: true,
+    description: "If defined, converts the OpenAPI specs to TypeScript source and writes it to this file"
+  })
+  .help().argv;
+
+//
+// Generate APIs
+//
+
+const env = initNunJucksEnvironment();
+generateApi(env, argv["api-spec"], argv["out-dir"], argv["ts-spec-file"]).then(
+  () => console.log("done"),
+  err => console.log(`Error: ${err}`)
+);
