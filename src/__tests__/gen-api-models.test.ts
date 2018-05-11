efcbc731e69b7aa51d86ff256b17863c87e03dbd
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-05-11 15:36:07 +0200

    test: added test for additiona propeties

--- src/__tests__/gen-api-models.test.ts
+++ src/__tests__/gen-api-models.test.ts
@@ -1,22 +1,29 @@
 import * as SwaggerParser from "swagger-parser";
 import { Spec, Schema } from "swagger-schema-official";
 
-import { initNunJucksEnvironment, renderDefinitionCode } from "../gen-api-models";
+import {
+  initNunJucksEnvironment,
+  renderDefinitionCode
+} from "../gen-api-models";
 
 const env = initNunJucksEnvironment();
 
 describe("gen-api-models", () => {
-
   it("should not generate duplicate imports", async () => {
     const spec: Spec = await SwaggerParser.bundle(`${__dirname}/api.yaml`);
     expect(spec.definitions).toBeDefined();
-    if(spec.definitions === undefined) {
+    if (spec.definitions === undefined) {
       fail("unexpected specs");
       return;
     }
     const profileDefinition = spec.definitions["Profile"];
     expect(profileDefinition).toBeDefined();
-    const code = await renderDefinitionCode(env, "Profile", profileDefinition);
+    const code = await renderDefinitionCode(
+      env,
+      "Profile",
+      profileDefinition,
+      false
+    );
     expect(code).toBeDefined();
     expect(code).toMatchSnapshot("dup-imports");
   });
@@ -24,8 +31,25 @@ describe("gen-api-models", () => {
   it("should handle NonNegativeNumbers", async () => {
     const spec: Spec = await SwaggerParser.bundle(`${__dirname}/api.yaml`);
     const profileDefinition = spec.definitions["NonNegativeNumberTest"];
-    const code = await renderDefinitionCode(env, "NonNegativeNumberTest", profileDefinition);
+    const code = await renderDefinitionCode(
+      env,
+      "NonNegativeNumberTest",
+      profileDefinition,
+      false
+    );
     expect(code).toMatchSnapshot("non-negative-numbers");
-  })
+  });
 
-})
+  it("should generate a dictionary from additionalProperties", async () => {
+    const spec: Spec = await SwaggerParser.bundle(`${__dirname}/api.yaml`);
+    const definition = spec.definitions["AdditionalPropsTest"];
+    const code = await renderDefinitionCode(
+      env,
+      "AdditionalPropsTest",
+      definition,
+      false
+    );
+    expect(code).toContain("t.dictionary");
+    expect(code).toMatchSnapshot("additional-properties");
+  });
+});
