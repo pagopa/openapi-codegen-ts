38e5aa105ff2adf978cbdd926d917c0f7690e895
Roberto Polli <robipolli@gmail.com>
2019-11-07 17:48:25 +0100

    Add test for request body. Related to #141.

--- src/__tests__/gen-api-models-oas3.test.ts
+++ src/__tests__/gen-api-models-oas3.test.ts
@@ -303,4 +303,24 @@ describe("gen-api-models", () => {
 
     expect(code.e1).toMatchSnapshot();
   });
+
+  it("should support generate requestbody", async () => {
+    const operation = spec.paths["/test-requestbody"].post;
+
+    const code = await renderOperation(
+      "post",
+      operation.operationId,
+      operation,
+      spec.components.parameters,
+      spec.components.securitySchemes,
+      [],
+      {},
+      "undefined",
+      "undefined",
+      true
+    );
+
+    expect(code.e1).toMatchSnapshot();
+  });
+
 });
