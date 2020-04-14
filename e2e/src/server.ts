0df438d4a015874217bd55de323ae05117a787d5
Emanuele De Cupis <ema.decu@gmail.com>
2020-04-14 17:19:47 +0200

    [#172075118] implemented prism correctly

--- e2e/src/server.ts
+++ e2e/src/server.ts
@@ -1,70 +1,59 @@
 /**
- * NOTE ON IMPLEMENTATION
  * This module abstracts the start and stop of a Prism mock server (https://github.com/stoplightio/prism).
- * Such mechanism should be implemented programmatically by importing Prism's needed modules, however this is not possible at the moment due to some flaws in Prism's dependency management. Please refer to the following threads for more info:
- * https://github.com/stoplightio/prism/issues/1070
- * https://github.com/balanza/didactic-waffle/pull/1
- * https://github.com/stoplightio/prism/pull/1072
  *
- * As for now, I implemented the feature by executing npx on separate processes. This is suboptimal in terms of resource consumption and execution time, and should be avoided as soon as Prims solves its issues.
- *
- * As soon as we can, we should refactor this module to call Prism as a module, ideally keeping this module public interface.
  */
 
-import { ChildProcess, spawn } from "child_process";
+import { createLogger } from "@stoplight/prism-core";
+import { getHttpOperationsFromResource } from "@stoplight/prism-http";
+import { createServer } from "@stoplight/prism-http-server";
 
-const servers = new Map<number, ChildProcess>();
+const servers = new Map<number, ReturnType<typeof createServer>>();
 
+/**
+ * Starts a mock server for a given specification
+ * @param apiSpecUrl path to the OpenApi specification document
+ * @param port port on which start the mock server
+ *
+ * @returns {Promise<void>} a resolving promise if the server is started correctly
+ */
 function startMockServer(apiSpecUrl: string, port: number = 4100) {
-  const [cmd, ...args] = [
-    "npx",
-    "-p",
-    "@stoplight/prism-cli",
-    "prism",
-    "mock",
-    apiSpecUrl,
-    "--port",
-    `${port}`
-  ];
-
-  console.log(`executing ${cmd} ${args.join(" ")}`);
   const startedAt = Date.now();
-
-  return new Promise((resolve, reject) => {
-    const proc = spawn(cmd, args, {
-      cwd: process.cwd(),
-      detached: true
-    });
-
-    proc.stdout.on("data", chunk => {
-      // wait for the server to be effectively listening
-      if (~chunk.toString().indexOf("listening on")) {
-        servers.set(port, proc);
-        console.log(`server started on port ${port} after ${Date.now() - startedAt}ms`);
-        resolve();
-      }
-    });
-    proc.on("error", e => {
-      reject(e);
-    });
-  });
+  return getHttpOperationsFromResource(apiSpecUrl)
+    .then(operations =>
+      createServer(operations, {
+        components: { logger: createLogger("test", { level: "silent" }) },
+        config: {
+          checkSecurity: true,
+          validateRequest: true,
+          validateResponse: true,
+          errors: false,
+          mock: { dynamic: false }
+        },
+        cors: true
+      })
+    )
+    .then(server => {
+      server.listen(port);
+      console.log(
+        `server started on port ${port} after ${Date.now() - startedAt}ms`
+      );
+      return server;
+    })
+    .then(server => servers.set(port, server));
 }
 
+/**
+ * Stop all the servers previously started
+ *
+ * @returns {Promise<void>} a resolving promise if the servers have been stopped correctly
+ */
 function stopAllServers() {
   console.log(`stopping servers on ports ${[...servers.keys()].join(", ")}`);
   return Promise.all(
-    [...servers.values()].map(
-      (proc: ChildProcess) =>
-        new Promise((resolve, reject) => {
-          try {
-            console.log("killing child process", proc.pid);
-            proc.on("close", () => resolve());
-            process.kill(-proc.pid);
-          } catch (error) {
-            reject(error);
-          }
-        })
-    )
+    [...servers.entries()].map(([port, server]) => {
+      console.log("shutting down server on port", port);
+      return server.close();
+    })
   ).then(() => servers.clear());
 }
 
