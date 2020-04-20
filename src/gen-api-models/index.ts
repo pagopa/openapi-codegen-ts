efe8c5e76332681cb889e6db0afbb063f2c8c5bf
Emanuele De Cupis <ema.decu@gmail.com>
2020-04-20 09:06:24 +0200

    fix lint errors

--- src/gen-api-models/index.ts
+++ src/gen-api-models/index.ts
@@ -28,7 +28,7 @@ function renderAsync(
       {
         definition,
         definitionName,
-        strictInterfaces,
+        strictInterfaces
       },
       (err, res) => {
         if (err) {
@@ -60,7 +60,7 @@ export async function renderDefinitionCode(
     strictInterfaces
   );
   const prettifiedCode = prettier.format(code, {
-    parser: "typescript",
+    parser: "typescript"
   });
   return prettifiedCode;
 }
@@ -177,7 +177,7 @@ const parseParameter = (
     return {
       name: `${param.name}${param.required ? "" : "?"}`,
       in: param.in,
-      type: specTypeToTs(param.type),
+      type: specTypeToTs(param.type)
     };
   }
   // Paratemer is declared as ref, we need to look it up
@@ -234,7 +234,7 @@ const parseParameter = (
   return {
     name: paramName,
     type: paramType,
-    in: paramIn,
+    in: paramIn
   };
 };
 
@@ -296,20 +296,20 @@ export const renderOperation = (
     headers,
     responses,
     importedTypes,
-    parameters,
+    parameters
   } = operationInfo;
 
   const requestType = `r.I${capitalize(method)}ApiRequestType`;
 
   const headersCode =
-    headers.length > 0 ? headers.map((_) => `"${_}"`).join("|") : "never";
+    headers.length > 0 ? headers.map(_ => `"${_}"`).join("|") : "never";
 
   const responsesType = responses
-    .map((r) => `r.IResponseType<${r.e1}, ${r.e2}>`)
+    .map(r => `r.IResponseType<${r.e1}, ${r.e2}>`)
     .join("|");
 
   const paramsCode = parameters
-    .map((param) => `readonly ${param.name}: ${param.type}`)
+    .map(param => `readonly ${param.name}: ${param.type}`)
     .join(",");
 
   const responsesDecoderCode = generateResponseDecoders
@@ -432,7 +432,7 @@ function renderDecoderCode({ responses, operationId }: IOperationInfo) {
   const defaultResponses = `
     export const ${defaultResponsesVarName} = {
       ${responses
-        .map((r) => `${r.e1}: ${r.e2 === "undefined" ? "t.undefined" : r.e2}`)
+        .map(r => `${r.e1}: ${r.e2 === "undefined" ? "t.undefined" : r.e2}`)
         .join(", ")}
     };
   `;
@@ -504,27 +504,27 @@ export function getAuthHeaders(
     security && security.length
       ? security
           .map((_: OpenAPIV2.SecurityRequirementObject) => Object.keys(_)[0])
-          .filter((_) => _ !== undefined)
+          .filter(_ => _ !== undefined)
       : undefined;
 
   const securityDefs =
     securityKeys !== undefined && securityDefinitions !== undefined
       ? // If we have both security and securityDefinitions defined, we extract
         // security items mapped to their securityDefinitions definitions.
-        securityKeys.map((k) => Tuple2(k, securityDefinitions[k]))
+        securityKeys.map(k => Tuple2(k, securityDefinitions[k]))
       : securityDefinitions !== undefined
-      ? Object.keys(securityDefinitions).map((k) =>
+      ? Object.keys(securityDefinitions).map(k =>
           Tuple2(k, securityDefinitions[k])
         )
       : [];
 
   return securityDefs
-    .filter((_) => _.e2 !== undefined)
-    .filter((_) => (_.e2 as OpenAPIV2.SecuritySchemeApiKey).in === "header")
-    .map((_) => {
+    .filter(_ => _.e2 !== undefined)
+    .filter(_ => (_.e2 as OpenAPIV2.SecuritySchemeApiKey).in === "header")
+    .map(_ => {
       const {
         name: headerName,
-        type: tokenType,
+        type: tokenType
       } = _.e2 as OpenAPIV2.SecuritySchemeApiKey; // Because _.e2 is of type OpenAPIV2.SecuritySchemeObject which is the super type of OpenAPIV2.SecuritySchemeApiKey. In the previous step of the chain we filtered so we're pretty sure _.e2 is of type OpenAPIV2.SecuritySchemeApiKey, but the compiler fails at it. I can add an explicit guard to the filter above, but I think the result is the same.
       return {
         headerName,
@@ -532,7 +532,7 @@ export function getAuthHeaders(
         in: "header" as "header", // this cast is needed otherwise "in" property will be recognize as string
         name: _.e1,
         type: "string",
-        tokenType,
+        tokenType
       };
     });
 }
@@ -567,8 +567,8 @@ const parseExtraParameters = (
                 headerName: param.in === "header" ? paramName : undefined,
                 in: param.in,
                 name: paramName,
-                type: specTypeToTs(param.type),
-              },
+                type: specTypeToTs(param.type)
+              }
             ];
           }
           return prev;
@@ -685,7 +685,7 @@ export const parseOperation = (
 
   const headers = [...contentTypeHeaders, ...authHeaders, ...extraHeaders];
 
-  const responses = Object.keys(operation.responses).map((responseStatus) => {
+  const responses = Object.keys(operation.responses).map(responseStatus => {
     const response = operation.responses[responseStatus];
     const typeRef = response.schema ? response.schema.$ref : undefined;
     const parsedRef = typeRef ? typeFromRef(typeRef) : undefined;
@@ -725,7 +725,7 @@ export const parseOperation = (
     importedTypes,
     path,
     consumes,
-    produces,
+    produces
   };
 };
 
@@ -758,7 +758,7 @@ export async function renderClientCode(
       "client.ts.njk",
       {
         operations,
-        spec,
+        spec
       },
       (err, code) => {
         if (err) {
@@ -767,7 +767,7 @@ export async function renderClientCode(
         } else {
           resolve(
             prettier.format(code || "", {
-              parser: "typescript",
+              parser: "typescript"
             })
           );
         }
@@ -796,11 +796,11 @@ export function parseAllOperations(
     : [];
 
   return Object.keys(api.paths)
-    .map((path) => {
+    .map(path => {
       const pathSpec = api.paths[path];
       const extraParameters = [
         ...parseExtraParameters(pathSpec),
-        ...globalAuthHeaders,
+        ...globalAuthHeaders
       ];
       return Object.keys(pathSpec)
         .map(
@@ -843,12 +843,12 @@ export async function generateApi(options: IGenerateApiOptions): Promise<void> {
     definitionsDirPath,
     strictInterfaces = false,
     defaultSuccessType = "undefined",
-    defaultErrorType = "undefined",
+    defaultErrorType = "undefined"
   } = options;
 
   const {
     generateRequestTypes = generateClient,
-    generateResponseDecoders = generateClient,
+    generateResponseDecoders = generateClient
   } = options;
 
   const env = initNunJucksEnvironment();
@@ -872,7 +872,7 @@ added this line
     await fs.writeFile(
       tsSpecFilePath,
       prettier.format(specCode, {
-        parser: "typescript",
+        parser: "typescript"
       })
     );
   }
@@ -943,14 +943,14 @@ added this line
           import * as r from "italia-ts-commons/lib/requests";
     
           ${Array.from(operationsImports.values())
-            .map((i) => `import { ${i} } from "./${i}";`)
+            .map(i => `import { ${i} } from "./${i}";`)
             .join("\n\n")}
     
           ${operationTypesCode}
         `;
 
     const prettifiedOperationsCode = prettier.format(operationsCode, {
-      parser: "typescript",
+      parser: "typescript"
     });
 
     const requestTypesPath = `${definitionsDirPath}/requestTypes.ts`;
@@ -973,7 +973,7 @@ added this line
 
 export function initNunJucksEnvironment(): nunjucks.Environment {
   nunjucks.configure({
-    trimBlocks: true,
+    trimBlocks: true
   });
   const env = new nunjucks.Environment(
     new nunjucks.FileSystemLoader(`${__dirname}/../../templates`)
@@ -1070,7 +1070,7 @@ export function initNunJucksEnvironment(): nunjucks.Environment {
   env.addFilter(
     "paramIn",
     (item: IParameterInfo[] | undefined, where: string) => {
-      return item ? item.filter((e) => e.in === where) : [];
+      return item ? item.filter(e => e.in === where) : [];
     }
   );
 
