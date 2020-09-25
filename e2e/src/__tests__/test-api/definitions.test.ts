e0cb42cd88560b01191eab03dd443800e1d201e2
Emanuele De Cupis <ema.decu@gmail.com>
2020-09-25 19:30:40 +0200

    e2e tests for numeric ranges

--- e2e/src/__tests__/test-api/definitions.test.ts
+++ e2e/src/__tests__/test-api/definitions.test.ts
@@ -4,6 +4,10 @@ import config from "../../config";
 import * as leaked from "leaked-handles";
 leaked.set({ debugSockets: true });
 
+import { IWithinRangeIntegerTag, IWithinRangeNumberTag } from "italia-ts-commons/lib/numbers";
+import { WithinRangeIntegerTest } from "../../generated/testapi/WithinRangeIntegerTest";
+import { WithinRangeNumberTest } from "../../generated/testapi/WithinRangeNumberTest";
+
 const { generatedFilesDir, isSpecEnabled } = config.specs.testapi;
 
 // if there's no need for this suite in this particular run, just skip it
@@ -82,4 +86,49 @@ describeSuite("Decoders generated from Test API spec defintions", () => {
       expect(result).toEqual(expected);
     });
   });
+
+  
+  //WithinRangeIntegerTest is defined min=0 max=10
+   it.each`
+     value        | expected
+     ${0}         | ${true}
+     ${-1}        | ${false}
+     ${5}         | ${true}
+     ${10}        | ${true}
+     ${11}        | ${false}
+     ${100}       | ${false}
+     ${undefined} | ${false}
+   `(
+     "should decode $value with WithinRangeIntegerTest",
+     ({ value, expected }) => {
+       const result = WithinRangeIntegerTest.decode(value);
+       expect(result.isRight()).toEqual(expected);
+       if (result.isRight()) {
+         // check definition
+         const _: IWithinRangeIntegerTag<0, 11> = result.value;
+       }
+     }
+   );
+
+   //WithinRangeNumberTest is defined min=0 max=10
+   it.each`
+     value  | expected
+     ${0}   | ${true}
+     ${-1}  | ${false}
+     ${5}   | ${true}
+     ${10}  | ${true}
+     ${11}  | ${false}
+     ${100} | ${false}
+     ${undefined} | ${false}
+   `(
+     "should decode $value with WithinRangeNumberTest",
+     ({ value, expected }) => {
+       const result = WithinRangeNumberTest.decode(value);
+       expect(result.isRight()).toEqual(expected);
+       if (result.isRight()) {
+         // check definition
+         const _: IWithinRangeNumberTag<0, 10> = result.value;
+       }
+     }
+   );
 });
