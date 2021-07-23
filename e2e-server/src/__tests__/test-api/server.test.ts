202a5124cd813f9e1d3a7255922093088cb08ef3
gquadrati <greta.quadrati@pagopa.it>
2021-07-23 20:33:35 +0200

    fix paramenter with dash handling

--- e2e-server/src/__tests__/test-api/server.test.ts
+++ e2e-server/src/__tests__/test-api/server.test.ts
@@ -8,14 +8,17 @@ import {
   ITestAuthBearerRequestHandler,
   ITestFileUploadRequestHandler,
   ITestMultipleSuccessRequestHandler,
+  ITestParameterWithDashRequestHandler,
   ITestSimpleTokenRequestHandler,
   setupTestAuthBearerEndpoint,
   setupTestFileUploadEndpoint,
-  setupTestMultipleSuccessEndpoint
+  setupTestMultipleSuccessEndpoint,
+  setupTestParameterWithDashEndpoint
 } from "../../generated/testapi/server";
 
 import * as request from "supertest";
 import { Message } from "../../generated/testapi/Message";
+import { isSome } from "fp-ts/lib/Option";
 
 describe("server", () => {
   let app: express.Express;
@@ -30,7 +33,6 @@ describe("server", () => {
 
   // ITestAuthBearerRequestHandler - TODO: query param
   it("should be able to build GetService Endpoint", async () => {
-
     const handler: ITestAuthBearerRequestHandler<{}> = ({}) => {
       return Promise.resolve(ResponseSuccessJson(undefined));
     };
@@ -44,21 +46,19 @@ describe("server", () => {
 
   // TestMultipleSuccess - 200
   it("should be able to build TestMultipleSuccess Endpoint", async () => {
-
     const handler: ITestMultipleSuccessRequestHandler<{}> = ({}) => {
-      return Promise.resolve(ResponseSuccessJson({id: "42"} as Message));
+      return Promise.resolve(ResponseSuccessJson({ id: "42" } as Message));
     };
 
     setupTestMultipleSuccessEndpoint(app, handler);
 
     const res = await request(app).get("/api/v1/test-multiple-success");
     expect(res.status).toBe(200);
-    expect(res.body).toMatchObject({id: "42"});
+    expect(res.body).toMatchObject({ id: "42" });
   });
 
   // TestMultipleSuccess - 202
   it("should be able to build TestMultipleSuccess Endpoint", async () => {
-
     const handler: ITestMultipleSuccessRequestHandler<{}> = ({}) => {
       return Promise.resolve(ResponseSuccessAccepted());
     };
@@ -72,7 +72,6 @@ describe("server", () => {
 
   // testFileUpload - 202 -- TODO: add formData
   it("should be able to build RestFileUpload Endpoint", async () => {
-
     const handler: ITestFileUploadRequestHandler<{}> = ({}) => {
       return Promise.resolve(ResponseSuccessJson(undefined));
     };
@@ -80,6 +79,26 @@ describe("server", () => {
     setupTestFileUploadEndpoint(app, handler);
 
     const res = await request(app).post("/api/v1/test-file-upload");
+    expect(res.status).toBe(200);
+    expect(res.body).toMatchObject({});
+  });
+
+  // test-parameter-with-dash
+  it("should be able to build test-parameter-with-dash Endpoint", async () => {
+    const handler: ITestParameterWithDashRequestHandler<{}> = ({
+      pathParam
+    }) => {
+      if (isSome(pathParam) && pathParam.value === "42")
+        return Promise.resolve(ResponseSuccessJson(undefined));
+      else return Promise.resolve(ResponseErrorInternal("pathparam undefined"));
+    };
+
+    setupTestParameterWithDashEndpoint(app, handler);
+
+    const res = await request(app).get("/api/v1/test-parameter-with-dash/42");
+
+    console.log(res.body);
+
     expect(res.status).toBe(200);
     expect(res.body).toMatchObject({});
   });
