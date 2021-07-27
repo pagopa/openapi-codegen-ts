8f46c8a2273bc7433cca34fb5fbfb96e4727506f
gquadrati <greta.quadrati@pagopa.it>
2021-07-27 18:05:51 +0200

    add post with body param test

--- e2e-server/src/__tests__/test-api/server.test.ts
+++ e2e-server/src/__tests__/test-api/server.test.ts
@@ -2,7 +2,8 @@ import {
   ProblemJson,
   ResponseErrorInternal,
   ResponseSuccessAccepted,
-  ResponseSuccessJson
+  ResponseSuccessJson,
+  ResponseSuccessRedirectToResource
 } from "@pagopa/ts-commons/lib/responses";
 import * as express from "express";
 import {
@@ -10,24 +11,32 @@ import {
   ITestFileUploadRequestHandler,
   ITestMultipleSuccessRequestHandler,
   ITestParameterWithDashRequestHandler,
+  ITestParameterWithReferenceRequestHandler,
   ITestSimpleTokenRequestHandler,
   ITestWithTwoParamsRequestHandler,
   setupTestAuthBearerEndpoint,
   setupTestFileUploadEndpoint,
   setupTestMultipleSuccessEndpoint,
   setupTestParameterWithDashEndpoint,
+  setupTestParameterWithReferenceEndpoint,
   setupTestWithTwoParamsEndpoint
 } from "../../generated/testapi/server";
 
 import * as request from "supertest";
 import { Message } from "../../generated/testapi/Message";
 import { isNone, isSome } from "fp-ts/lib/Option";
+import { MessageBodyMarkdown } from "../../generated/testapi/MessageBodyMarkdown";
+import { MessageSubject } from "../../../../e2e/src/generated/testapi/MessageSubject";
+import * as bodyParser from "body-parser";
 
 describe("server", () => {
   let app: express.Express;
   beforeEach(() => {
     //Setup express
     app = express();
+    app.use(bodyParser.json());
+    // Parse an urlencoded body.
+    app.use(bodyParser.urlencoded({ extended: true }));
   });
 
   afterEach(() => {
@@ -106,13 +115,75 @@ describe("server", () => {
     expect(res.body).toMatchObject({});
   });
 
-  // est-parameter-with-two-dash
+  // test-parameter-with-reference - Middleware validation error
+  it("should be able to build test-parameter-with-reference Endpoint (Middleware validation result)", async () => {
+    const handler: ITestParameterWithReferenceRequestHandler<{}> = ({
+      createdMessage
+    }) => {
+      return Promise.resolve(
+        ResponseSuccessRedirectToResource(undefined, "", undefined)
+      );
+    };
+
+    setupTestParameterWithReferenceEndpoint(app, handler);
+
+    const res = await await request(app)
+      .post("/api/v1/test-parameter-with-reference");
+
+    console.log(res.body);
+    console.log(res.headers);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toMatchObject({
+      detail:
+        "value [undefined] at [root.0.id] is not a valid [string]\n" +
+        "value [undefined] at [root.0.content] is not a valid [Exact<MessageContent>]",
+      status: 400,
+      title: "Invalid Exact<Message>"
+    });
+  });
+
+  // Post with  body - Success
+  it("should be able to build test-parameter-with-reference Endpoint", async () => {
+    const handler: ITestParameterWithReferenceRequestHandler<{}> = ({
+      createdMessage
+    }) => {
+      return Promise.resolve(
+        ResponseSuccessRedirectToResource(undefined, "anUrl", undefined)
+      );
+    };
+
+    setupTestParameterWithReferenceEndpoint(app, handler);
+
+    const message: Message = {
+      id: "anId",
+      content: {
+        markdown: "a".repeat(81) as MessageBodyMarkdown,
+        subject: "s".repeat(80) as MessageSubject
+      }
+    };
+    const res = await request(app)
+      .post("/api/v1/test-parameter-with-reference")
+      .send(message);
+
+    console.log(res.body);
+    console.log(res.headers);
+
+    expect(res.status).toBe(201);
+    expect(res.get("Location")).toEqual("anUrl");
+  });
+
+  // test-parameter-with-two-dash
   it("should be able to build test-parameter-with-two-dash Endpoint", async () => {
     const handler: ITestWithTwoParamsRequestHandler<{}> = ({
       firstParam,
       secondParam
     }) => {
-      if (isSome(firstParam) && secondParam === 42)
+      if (
+        isSome(firstParam) &&
+        isSome(secondParam) &&
+        secondParam.value === "42"
+      )
         return Promise.resolve(ResponseSuccessJson(undefined));
       else return Promise.resolve(ResponseErrorInternal("pathparam undefined"));
     };
@@ -125,46 +196,5 @@ describe("server", () => {
 
     expect(res.status).toBe(200);
     expect(res.body).toMatchObject({});
-  });
-
-  // test-parameter-with-two-dash - 400 ProblemJson result
-  it("should be able to build test-parameter-with-dash Endpoin - Validation error ( > 100)", async () => {
-    const handler: ITestWithTwoParamsRequestHandler<{}> = ({}) =>
-      Promise.resolve(ResponseSuccessJson(undefined));
-    setupTestWithTwoParamsEndpoint(app, handler);
-
-    const res = await request(app).get(
-      "/api/v1/test-two-path-params/1/101"
-    );
-
-    expect(res.status).toBe(400);
-    expect(res.body).toMatchObject({
-      detail: 'value [\"101\"] at [root.0] is not a valid [number >= 0 and < 100]\nvalue [\"101\"] at [root.1] is not a valid [100]',
-      status: 400,
-      title: "Invalid (number >= 0 and < 100 | 100)"
-    });
-  });
-
-  // test-parameter-with-two-dash - 400 ProblemJson result
-  it("should be able to build test-two-path-params Endpoint returning a validation error", async () => {
-    const handler: ITestWithTwoParamsRequestHandler<{}> = ({
-      firstParam,
-      secondParam
-    }) => {
-      return Promise.resolve(ResponseSuccessJson(undefined));
-    };
-
-    setupTestWithTwoParamsEndpoint(app, handler);
-
-    const res = await request(app).get(
-      "/api/v1/test-two-path-params/1/aString"
-    );
-
-    expect(res.status).toBe(400);
-    expect(res.body).toMatchObject({
-      detail: 'value [\"aString\"] at [root.0] is not a valid [number >= 0 and < 100]\nvalue [\"aString\"] at [root.1] is not a valid [100]',
-      status: 400,
-      title: "Invalid (number >= 0 and < 100 | 100)"
-    });
   });
 });
